becf5ac668e2fb3407d2a71257d450065ff99cb7
kap-akos <kappelakos04@gmail.com>
2024-10-04 20:43:21 +0200

    Add files via upload

---
+++ sakk.js
@@ -0,0 +1,73 @@
+var kiraly = document.getElementById("kiraly_szoveg");
+kiraly.style.visibility = 'hidden';
+
+function kiraly_kep(){
+    document.getElementById("kiraly_szoveg")
+        if (kiraly_szoveg.style.visibility == "hidden"){
+            kiraly_szoveg.style.visibility = "visible"
+        }else{
+            kiraly_szoveg.style.visibility = "hidden"
+        }
+
+}
+var vezer = document.getElementById("vezer_szoveg");
+vezer.style.visibility = 'hidden';
+
+function vezer_kep(){
+    document.getElementById("vezer_szoveg")
+        if (vezer_szoveg.style.visibility == "hidden"){
+            vezer_szoveg.style.visibility = "visible"
+        }else{
+            vezer_szoveg.style.visibility = "hidden"
+        }
+
+}
+var futo = document.getElementById("futo_szoveg");
+futo.style.visibility = 'hidden';
+
+function futo_kep(){
+    document.getElementById("futo_szoveg")
+        if (futo_szoveg.style.visibility == "hidden"){
+            futo_szoveg.style.visibility = "visible"
+        }else{
+            futo_szoveg.style.visibility = "hidden"
+        }
+
+}
+var lo = document.getElementById("lo_szoveg");
+lo.style.visibility = 'hidden';
+
+function lo_kep(){
+    document.getElementById("lo_szoveg")
+        if (lo_szoveg.style.visibility == "hidden"){
+            lo_szoveg.style.visibility = "visible"
+        }else{
+            lo_szoveg.style.visibility = "hidden"
+        }
+
+}
+var bastya = document.getElementById("bastya_szoveg");
+bastya.style.visibility = 'hidden';
+
+function bastya_kep(){
+    document.getElementById("bastya_szoveg")
+        if (bastya_szoveg.style.visibility == "hidden"){
+            bastya_szoveg.style.visibility = "visible"
+        }else{
+            bastya_szoveg.style.visibility = "hidden"
+        }
+
+}
+var gyalog = document.getElementById("gyalog_szoveg");
+gyalog.style.visibility = 'hidden';
+
+function gyalog_kep(){
+    document.getElementById("gyalog_szoveg")
+        if (gyalog_szoveg.style.visibility == "hidden"){
+            gyalog_szoveg.style.visibility = "visible"
+        }else{
+            gyalog_szoveg.style.visibility = "hidden"
+        }
+
+}
+
